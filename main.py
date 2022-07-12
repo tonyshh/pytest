f4d8621ea3e645819cee9297be29bc96120b5958
shalin <shalin.anton8@gmail.com>
2022-07-12 18:54:02 +1000

    add file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello world!')
+price = int(input())
+coins = 0
+while price >= 25:
+    coins += 1
+    price = price - 25
+while price >= 10:
+    coins += 1
+    price -= 10
+while price >= 5:
+    coins += 1
+    price -= 5
+while 1 <= price < 5:
+    coins += 1
+    price -= 1
+print(coins)
